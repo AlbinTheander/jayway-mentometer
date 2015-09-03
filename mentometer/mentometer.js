b81b1170fbc9cbbae405c8a6e8e76a4e5b225bf2
eliseellerstedt <Admin@Jayways-MacBook-Pro.local>
2015-09-03 14:30:01 +0200

    Saves votes in db

--- mentometer/mentometer.js
+++ mentometer/mentometer.js
@@ -1,21 +1,22 @@
+Votes = new Mongo.Collection("votes");
+
+
+
 Router.route('/voting', function(){
 	this.render('Voting');
 });
 
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
+ 
+  Template.Voting.events({
+    'click .yes': function () {
       // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      Votes.insert({
+      	result: "yes",
+      	createdAt: new Date()
+      });
+      alert('Du röstade ja!');
     }
   });
 }
